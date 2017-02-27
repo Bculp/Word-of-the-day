fc0e73100e74e495032b648a8efeb396794de991
Brandon Culp <bculp513@gmail.com>
2017-02-27 01:22:10 -0500

    synonyms and antonyms are properly retrieved and displayed if they exist

--- browser/app/components/MainComponent.js
+++ browser/app/components/MainComponent.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default class MainComponent extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {word: '', partOfSpeech: '', definition: '', exampleUses: [] };
+		this.state = {word: '', partOfSpeech: '', definition: '', exampleUses: [], synonyms: [], antonyms: [] };
 	}
 
 	componentDidMount() {
@@ -12,16 +12,36 @@ export default class MainComponent extends React.Component {
 	}
 
 	getWord() {
-		axios.get('api/keys')
+		axios.get('api/getWord')
 		.then(res => res.data)
 		.then(info => {
 			let instance = info[0];
-			this.setState({word: instance.word});
-			this.setState({partOfSpeech: instance.partOfSpeech});
-			this.setState({definition: instance.text});
-			this.setState({exampleUses: instance.exampleUses})
+			this.setState({
+				word: instance.word,
+				partOfSpeech: instance.partOfSpeech,
+				definition: instance.text,
+				exampleUses: instance.exampleUses
+			})
+			this.getRelatedWords(instance.word)
 		})
-		.catch('error getting keys')
+		.catch(error => console.error(error))
+	}
+
+	getRelatedWords(word) {
+		axios.get(`api/getRelatedWords/${word}`)
+		.then(res => res.data)
+		.then(relatedWords => {
+			let synonyms = [], antonyms = [];
+			relatedWords && relatedWords.map(instance => {
+				if (instance.relationshipType === 'synonym') {
+					synonyms = instance.words;
+				} else if (instance.relationshipType === 'antonym') {
+					antonyms = instance.words;
+				}
+			})
+			this.setState({synonyms, antonyms})
+		})
+		.catch(error => console.error(error))
 	}
 
 	render() {
@@ -31,6 +51,8 @@ export default class MainComponent extends React.Component {
 					<h1>{this.state.word ? `${this.state.word} - ${this.state.partOfSpeech}` : ''}</h1>
 					<h2>{this.state.definition ? `Definition: ${this.state.definition}`: ''}</h2>
 					<h4>{this.state.exampleUses.length > 0 ? `Examples: ${this.state.exampleUses}` : ''}</h4>
+					<h5>{this.state.synonyms.length > 0 ? `Synonyms: ${this.state.synonyms}` : ''}</h5>
+					<h5>{this.state.antonyms > 0 ? `Antonyms: ${this.state.antonyms}` : ''}</h5>
 				</div>
 			</div>
 		)
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -7,7 +7,7 @@ module.exports = router;
 // can run another query to get syns and ants when push that button or when initial query runs.
 
 // by using wordnik. my limit is 15k per HOUR so no need to have a counter although i can't store their data..
-router.get('/keys', (req, res, next) => {
+router.get('/getWord', (req, res, next) => {
 	// get a random word
 	axios.get(`http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=${process.env['API-KEY']}`)
 	.then(res => res.data)
@@ -15,11 +15,19 @@ router.get('/keys', (req, res, next) => {
 		// get definition, part of speech and examples if they exist
 		axios.get(`http://api.wordnik.com:80/v4/word.json/${result.word}/definitions?limit=1&includeRelated=false&useCanonical=false&includeTags=false&api_key=${process.env['API-KEY']}`)
 		.then(res => res.data)
-		.then(result => res.send(result))
+		.then(info => res.send(info))
 		.catch(next)
 	})
 	.catch(next)
 })
+
+router.get('/getRelatedWords/:word', (req, res, next) => {
+	axios.get(`http://api.wordnik.com:80/v4/word.json/${req.params.word}/relatedWords?useCanonical=false&limitPerRelationshipType=10&api_key=${process.env['API-KEY']}`)
+	.then(res => res.data)
+	.then(relatedWords => res.send(relatedWords))
+	.catch(next)
+})
+
 
 router.use((req, res) => {
 	res.sendStatus(404);
