322b10af16e05bf79018ded8c89918f7eb514011
Brandon Culp <bculp513@gmail.com>
2017-03-05 14:57:20 -0500

    fixed synonyms/antonyms showing and not being centered

--- public/testing.js
+++ public/testing.js
@@ -1,11 +1,9 @@
 const axios = require('axios');
-// const containerEl = document.querySelector('.flex-container');
 const wordEl = document.querySelector('#word')
 const partOfSpeechEl = document.querySelector('#partOfSpeech')
 const definitionEl = document.querySelector('#definition')
 const listEl = document.querySelector('.list')
-const synonymsEl = document.querySelector('#synonyms');
-const antonymsEl = document.querySelector('#antonyms');
+let synonymsFound = false, antonymsFound = false;
 
 document.title = "Word of the Day"
 
@@ -13,7 +11,7 @@ let state = {word: '', partOfSpeech: '', definition: '', synonyms: [], antonyms:
 
 function getWord() {
 
-	synonymsEl.innerHTML = '';
+	// synonymsEl.innerHTML = '';
 	console.log('get word running');
 	axios.get('api/getWord')
 	.then(res => res.data)
@@ -21,7 +19,6 @@ function getWord() {
 		let instance = info[0];
 		state.word = instance.word[0].toUpperCase() + instance.word.slice(1);
 		state.partOfSpeech = `[${instance.partOfSpeech}]`;
-		// state.partOfSpeech = instance.partOfSpeech ? instance.partOfSpeech[0].toUpperCase() + instance.partOfSpeech.slice(1) : '';
 		state.definition = instance.text;
 		console.log('get word finished')
 		getRelatedWords(instance.word)
@@ -36,11 +33,9 @@ function getRelatedWords(word) {
 		let synonyms = [], antonyms = [];
 		relatedWords && relatedWords.map(instance => {
 			if (instance.relationshipType === 'synonym') {
-				console.log(instance.words)
 				synonyms = instance.words.slice(0,3);
 			} else if (instance.relationshipType === 'antonym') {
 					antonyms = instance.words.slice(0,3);
-					console.log(instance.words)
 			}
 		})
 
@@ -48,26 +43,36 @@ function getRelatedWords(word) {
 		partOfSpeechEl.textContent = state.partOfSpeech;
 		definitionEl.textContent = state.definition;
 
-		if (synonyms.length < 1) synonyms = ['None found'];
-		if (antonyms.length < 1) antonyms = ['None found'];
 		state.synonyms = synonyms;
 		state.antonyms = antonyms;
 
-		synonymsEl.textContent = 'Synonyms';
-		state.synonyms.map(synonym => {
-			let li = document.createElement('li');
-			li.textContent = synonym[0].toUpperCase() + synonym.slice(1);
-			li.classList.add('list');
-			synonymsEl.appendChild(li);
-		})
+		if (state.synonyms.length > 0) synonymsFound = true;
+		if (state.antonyms.length > 0) antonymsFound = true;
 
-		antonymsEl.textContent = 'Antonyms';
-		state.antonyms.map(antonym => {
-			let li = document.createElement('li');
-			li.textContent = antonym[0].toUpperCase() + antonym.slice(1);
-			li.classList.add('list');
-			antonymsEl.appendChild(li);
-		})
+		if (synonymsFound) {
+			let synonymsEl = document.createElement('ul');
+			synonymsEl.textContent = 'Synonyms';
+			synonymsEl.classList.add('no-padding-list');
+			listEl.appendChild(synonymsEl);
+			state.synonyms.map(synonym => {
+				let li = document.createElement('li');
+				li.textContent = synonym[0].toUpperCase() + synonym.slice(1);
+				synonymsEl.appendChild(li);
+			})
+		}
+
+		if (antonymsFound) {
+			let antonymsEl = document.createElement('ul');
+			antonymsEl.textContent = 'Antonyms';
+			if (synonymsFound && antonymsFound) antonymsEl.classList.add('normal-list');
+			else antonymsEl.classList.add('no-padding-list');
+			listEl.appendChild(antonymsEl);
+			state.antonyms.map(antonym => {
+				let li = document.createElement('li');
+				li.textContent = antonym[0].toUpperCase() + antonym.slice(1);
+				antonymsEl.appendChild(li);
+			})
+		}
 
 	})
 	.catch(error => console.error(error))
